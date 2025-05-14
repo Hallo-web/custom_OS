d0efd40ab75a8ee922e715fc54aa46aad899811e
Philipp Marco Schwarz <philipp.schwarz@student.tugraz.at>
2025-05-14 16:13:32 +0200

    added some things but i need to improve the boot up splash screen

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-
-TARGET = voidblade_os
+TARGET = osiris_os
 ISO = $(TARGET).iso
+QEMU = qemu-system-x86_64
 
 CC = gcc
 LD = ld
@@ -10,27 +10,37 @@ LDFLAGS = -T linker.ld -m elf_i386
 
 all: $(ISO)
 
+# Compile the boot assembly file
 boot.o: boot/boot.asm
 	$(AS) -f elf32 boot/boot.asm -o boot.o
 
+# Compile the kernel C file
 kernel.o: src/kernel.c
 	$(CC) $(CFLAGS) -c src/kernel.c -o kernel.o
 
+# Create the binary from object files
 kernel.bin: boot.o kernel.o
 	$(LD) $(LDFLAGS) -o kernel.bin boot.o kernel.o
 
+# Create ISO image
 $(ISO): kernel.bin
 	mkdir -p isodir/boot/grub
 	cp kernel.bin isodir/boot/kernel.bin
 	echo 'set timeout=0' > isodir/boot/grub/grub.cfg
 	echo 'set default=0' >> isodir/boot/grub/grub.cfg
-	echo 'menuentry "VoidBlade OS" {' >> isodir/boot/grub/grub.cfg
+	echo 'menuentry "O.S.I.R.I.S" {' >> isodir/boot/grub/grub.cfg
 	echo '  multiboot /boot/kernel.bin' >> isodir/boot/grub/grub.cfg
 	echo '  boot' >> isodir/boot/grub/grub.cfg
 	echo '}' >> isodir/boot/grub/grub.cfg
-	xorriso -as mkisofs -R -b boot/grub/stage2_eltorito \
-	  -no-emul-boot -boot-load-size 4 -boot-info-table \
-	  -o $(ISO) isodir
+	grub-mkrescue -o $(ISO) isodir
 
+# Clean up object files and binaries
 clean:
 	rm -rf *.o *.bin isodir *.iso
+
+# Run the ISO with QEMU
+run: $(ISO)
+	$(QEMU) -cdrom $(ISO)
+
+# Clean, rebuild, and run the project
+test: clean all run
--- src/kernel.c
+++ src/kernel.c
@@ -1,12 +1,55 @@
+__attribute__((section(".multiboot")))
+const unsigned int multiboot_header[] = {
+    0x1BADB002,   // magic
+    0x00,         // flags
+    -(0x1BADB002) // checksum = -(magic + flags)
+};
 
-void kernel_main() {
-    const char *str = "VoidBlade OS: Ready for world domination";
-    char *vidptr = (char*)0xb8000;  // VGA text mode memory
-    unsigned int i = 0;
-    while(str[i] != '\0') {
-        vidptr[i*2] = str[i];
-        vidptr[i*2+1] = 0x07;  // Light gray on black
-        i++;
+void clear_screen()
+{
+    char *video_memory = (char *)0xb8000;
+    for (int i = 0; i < 80 * 25 * 2; i += 2)
+    {
+        video_memory[i] = ' ';
+        video_memory[i + 1] = 0x07;
     }
-    while (1);
+}
+
+void print_centered(const char *str, int row)
+{
+    int len = 0;
+    while (str[len])
+        len++;
+
+    int col = (80 - len) / 2;
+    int offset = (row * 80 + col) * 2;
+    char *video_memory = (char *)0xb8000;
+}
+
+void splashScreen()
+{
+    const char *message = "O.S.I.R.I.S";
+    int offset = 0;
+
+    print_centered(message, 5);
+}
+
+void kernel_main()
+{
+    clear_screen();
+    const char *message = "Booting...";
+    
+    char *video_memory = (char *)0xb8000;
+    splashScreen();
+    int offset = 0;
+
+    for (int i = 0; message[i] != '\0'; ++i)
+    {
+        video_memory[i * 2] = message[i];
+        video_memory[i * 2 + 1] = 0x0F; // white on black
+    }
+
+    while (1)
+    {
+    } // Halt the CPU
 }
